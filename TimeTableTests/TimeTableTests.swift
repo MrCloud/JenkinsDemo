cb2f110959aee06b0bf3d2d27c94e9f63be45640
Florian PETIT <florianp37@me.com>
2017-05-12 19:47:55 +0200

    add failing test

--- TimeTableTests/TimeTableTests.swift
+++ TimeTableTests/TimeTableTests.swift
@@ -34,6 +34,13 @@ class TimeTableTests: XCTestCase {
         }
     }
     
+    func testFailingExample() {
+        // This is an example of a failing functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(false)
+    }
+
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         let timeTable = TimeTable()
